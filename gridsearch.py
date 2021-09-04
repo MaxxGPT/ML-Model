0caf426bbd6bd702567f21d46a33976f96b5041d
MaxxABillion <afpilot2020@gmail.com>
2021-09-04 18:29:46 -0400

    adding gridsearch file

--- gridsearch.py
+++ gridsearch.py
@@ -8,33 +8,32 @@ from sklearn.decomposition import NMF, LatentDirichletAllocation
 
 import logging
 import logging.handlers as handlers
-import time
+import time, os
 
 class GridSearch:
     def __init__(self,tf):
         self.tf=tf
-        self.search_components=[2, 3, 4]
+        self.search_components=[2, 3, 4, 5, 6]
+        self.search_learning_decay=[0.6, 0.8]
         self.logger=self.init_logger()
         self.logger.info("init...")
-        print("Grid Search init....")
-    
+
     def init_logger(self):
         logger = logging.getLogger('GridSearch')
         logger.setLevel(logging.INFO)
         return logger
 
-    def start(self):   
+    def start(self):
         self.logger.info("Started...")
-        print("Grid Search Started....")
         no_topics=4
         t = time.process_time()
         try:
             # Define Search Param
             search_params = {'n_components': self.search_components}
             # Init the Model
-            lda = LatentDirichletAllocation(max_iter=5, learning_method='online', learning_offset=50.,random_state=0)
+            lda = LatentDirichletAllocation(max_iter=5, learning_method='online', learning_offset=50.,random_state=0, n_jobs = os.cpu_count() // 1.5)
             # Init Grid Search Class
-            model = GridSearchCV(lda, param_grid=search_params,verbose=10)
+            model = GridSearchCV(lda, param_grid=search_params,verbose=10, n_jobs = os.cpu_count() // 1.5)
             # Do the Grid Search
             model.fit(self.tf)
             # Best Model
@@ -42,13 +41,9 @@ class GridSearch:
             # Model Parameters
             self.logger.info("Completed...")
             self.logger.info(model.best_params_)
-            print("Grid Search Completed....")
             no_topics=model.best_params_["n_components"]
         except Exception as e:
             self.logger.error(e)
-            print(e)
         elapsed_time = time.process_time() - t
         self.logger.info("Execution Time[Seconds] "+str(elapsed_time))
-        print("Execution Time[Seconds] "+str(elapsed_time))
-
         return no_topics
