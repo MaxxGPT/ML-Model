872f33d1eb31f583157e491aa850a99da3510468
MaxxABillion <afpilot2020@gmail.com>
2021-09-09 11:11:41 -0400

    adding predict

--- predict.py
+++ predict.py
@@ -1,7 +1,11 @@
 from text_analyser import TextAnalyser
+from sentiment_analysis import SentimentAnalysis
 from lda import Lda
 
 if __name__ == "__main__":
     print("Text Analysis Started....")
     textanalyser=TextAnalyser()
     textanalyser.start()
+    print("Sentiment Analysis started")
+    sentiment_analysis = SentimentAnalysis()
+    sentiment_analysis.start()
