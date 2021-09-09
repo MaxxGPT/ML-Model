c34f8444e657ec2b62c5561ac292f3c8b913664e
MaxxABillion <afpilot2020@gmail.com>
2021-09-09 11:09:51 -0400

    adding predict

--- predict.py
+++ predict.py
@@ -2,6 +2,6 @@ from text_analyser import TextAnalyser
 from lda import Lda
 
 if __name__ == "__main__":
-    print("Topic Prediction Started....")
+    print("Text Analysis Started....")
     textanalyser=TextAnalyser()
     textanalyser.start()
